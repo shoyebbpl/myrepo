5050ccb185ff751ceccedd99d20943f897faa33c
khan <msk.bpl1@gmail.com>
2024-08-26 22:04:52 +0530

    humneh modify krdiya hai

--- provider.tf
+++ provider.tf
@@ -13,3 +13,18 @@ provider "azurerm" {
   }
    subscription_id = "e04cd4cd-630a-43a4-b6b3-32fb9fd41d45"
 }
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.0.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    
+  }
+   subscription_id = "e04cd4cd-630a-43a4-b6b3-32fb9fd41d45"
+}
